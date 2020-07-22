33c8dc085913f9653044da5179b66cd5423f544e
Thamaraiselvan Balakrishnan <thamarai114nkl@gmail.com>
2020-07-23 03:41:05 +0530

    updation done

---
+++ setTimeOut/setTimeOut.js
@@ -0,0 +1,15 @@
+ var Message=()=>{
+    let a = document.getElementById("demo");
+    setTimeout(()=>{a.value='10..'},1000);
+    setTimeout(()=>{a.value='09..'},2000);
+    setTimeout(()=>{a.value='08..'},3000);
+    setTimeout(()=>{a.value='07..'},4000);
+    setTimeout(()=>{a.value='06..'},5000);
+    setTimeout(()=>{a.value='05..'},6000);
+    setTimeout(()=>{a.value='04..'},7000);
+    setTimeout(()=>{a.value='03..'},8000);
+    setTimeout(()=>{a.value='02..'},9000);
+    setTimeout(()=>{a.value='01..'},10000);
+    setTimeout(()=>{a.value='Happy morning Anu..'},11000);
+}
+Message()
